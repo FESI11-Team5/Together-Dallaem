044474c0b3f3bb0050de664826ae082fa657bcce
bbagbbagn2 <pyoungh999@naver.com>
2025-09-26 14:42:43 +0900

    Chore: jsDoc 주석 추가

--- src/apis/users/index.ts
+++ src/apis/users/index.ts
@@ -2,19 +2,37 @@ import { getRequest, putRequest } from '@/apis';
 import { UserInfo } from '@/types/user';
 
 interface UpdateUserInfoProps {
-    companyName?: string;
+	companyName?: string;
 	image?: string;
-    email?: string;
+	email?: string;
 }
 
-// 사용자 정보 조회
+/**
+ * 팀의 사용자 정보를 조회합니다.
+ *
+ * @param {string} teamId - 조회할 팀 ID
+ * @returns {Promise<UserInfo>} 조회된 사용자 정보
+ *
+ * @example
+ * getUserInfo('1').then(user => console.log(user.name));
+ */
 export const getUserInfo = (teamId: string) => {
 	return getRequest<UserInfo>({
 		path: `/${teamId}/auths/user`
 	});
 };
 
-// 사용자 정보 수정
+/**
+ * 팀의 사용자 정보를 수정합니다.
+ *
+ * @param {string} teamId - 수정할 팀 ID
+ * @param {UpdateUserInfoProps} updatedData - 수정할 사용자 정보
+ * @returns {Promise<UserInfo>} 수정 후 반환된 사용자 정보
+ *
+ * @example
+ * updateUserInfo('123', { companyName: 'New Company' })
+ *   .then(user => console.log(user.companyName));
+ */
 export const updateUserInfo = (teamId: string, updatedData: UpdateUserInfoProps) => {
 	return putRequest<UserInfo, UpdateUserInfoProps>({
 		path: `/${teamId}/auths/user`,
