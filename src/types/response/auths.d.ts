7eee642c25cce2828ed042fc6e2e91d7cc554ae3
y0unj1NoH <nohyoong711@gmail.com>
2025-09-26 14:46:47 +0900

    TD-74 Feat: 타입/스키마 정의

--- src/types/response/auths.d.ts
+++ src/types/response/auths.d.ts
@@ -12,3 +12,14 @@ export interface signupRequest {
 export interface signupResponse {
 	message: string;
 }
+export interface signinRequest {
+	/** 이메일 */
+	email: string;
+	/** 비밀번호 */
+	password: string;
+}
+
+export interface signinResponse {
+	/** Access Token */
+	token: string;
+}
