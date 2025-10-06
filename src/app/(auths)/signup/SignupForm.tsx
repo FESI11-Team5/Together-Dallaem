9a41e8c5c03f7f6a44e604a3efe7619b0cee651d
y0unj1NoH <nohyoong711@gmail.com>
2025-10-06 20:55:30 +0900

    TD-125 Feat: 페이지 구조 & RHF 바인딩 (TDD)

--- src/app/(auths)/signup/SignupForm.tsx
+++ src/app/(auths)/signup/SignupForm.tsx
@@ -1,21 +1,74 @@
 'use client';
 import BasicButton from '@/components/commons/BasicButton';
 import { signupLabel, signupPlaceholders } from '@/constants/form';
+import { zodResolver } from '@hookform/resolvers/zod';
 import Link from 'next/link';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
 import { InputWithLabel } from './InputWithLabel';
+import { zodSchema } from './signupValidator/signupValidator.zod';
+// import { yupSchema } from './signupValidator/signupValidator.yup';
+// import { yupResolver } from '@hookform/resolvers/yup';
+// import type { SignupFormValues } from './signupValidator/signupValidator';
+
+type SignupFormValues = z.infer<typeof zodSchema>;
 
 export function SignupForm() {
+	const {
+		register,
+		handleSubmit,
+		formState: { errors, isSubmitting, isValid, isDirty }
+	} = useForm<SignupFormValues>({
+		resolver: zodResolver(zodSchema),
+		// resolver: yupResolver(yupSchema),
+		mode: 'onBlur'
+	});
+
+	// TODO: API 연동
+	const onSubmit = (data: SignupFormValues) => {
+		console.log(data);
+	};
+
 	return (
-		<form className="flex w-full flex-col gap-10">
+		<form className="flex w-full flex-col gap-10" onSubmit={handleSubmit(onSubmit)}>
 			<div className="flex flex-col gap-6">
-				<InputWithLabel label={signupLabel.name} placeholder={signupPlaceholders.name} />
-				<InputWithLabel label={signupLabel.email} placeholder={signupPlaceholders.email} />
-				<InputWithLabel label={signupLabel.companyName} placeholder={signupPlaceholders.companyName} />
-				<InputWithLabel label={signupLabel.password} placeholder={signupPlaceholders.password} isPassword />
-				<InputWithLabel label={signupLabel.confirm} placeholder={signupPlaceholders.confirm} isPassword />
+				<InputWithLabel
+					label={signupLabel.name}
+					placeholder={signupPlaceholders.name}
+					register={register('name')}
+					invalidText={errors.name?.message}
+				/>
+				<InputWithLabel
+					label={signupLabel.email}
+					placeholder={signupPlaceholders.email}
+					register={register('email')}
+					invalidText={errors.email?.message}
+				/>
+				<InputWithLabel
+					label={signupLabel.companyName}
+					placeholder={signupPlaceholders.companyName}
+					register={register('companyName')}
+					invalidText={errors.companyName?.message}
+				/>
+				<InputWithLabel
+					label={signupLabel.password}
+					placeholder={signupPlaceholders.password}
+					isPassword
+					register={register('password')}
+					invalidText={errors.password?.message}
+				/>
+				<InputWithLabel
+					label={signupLabel.confirm}
+					placeholder={signupPlaceholders.confirm}
+					isPassword
+					register={register('confirm')}
+					invalidText={errors.confirm?.message}
+				/>
 			</div>
 			<div className="flex flex-col gap-6">
-				<BasicButton>확인</BasicButton>
+				<BasicButton className="w-full" isActive={isValid && !isSubmitting && isDirty}>
+					확인
+				</BasicButton>
 				<div className="flex items-center justify-center gap-1">
 					<p className="text-base font-medium">이미 회원이신가요?</p>
 					<Link href="/login" className="text-orange-600 underline">
