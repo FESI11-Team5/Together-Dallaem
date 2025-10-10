bfb6d5afc924611a1349d133cff5503572df67d9
y0unj1NoH <nohyoong711@gmail.com>
2025-10-10 13:35:39 +0900

    Chore: legacy 제거

--- src/app/(auths)/login/loginSchema.ts
+++
@@ -1,7 +0,0 @@
-import { loginErrors } from '@/constants/error';
-import * as z from 'zod';
-
-export const loginSchema = z.object({
-	email: z.email({ error: loginErrors.emailInvalid }),
-	password: z.string().min(8, { error: loginErrors.passwordTooShort })
-});
