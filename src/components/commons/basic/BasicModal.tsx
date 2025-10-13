d85962e4719713b7f7cbf57cfa977e4ed9f8a2d9
annann23 <annann23@naver.com>
2025-10-13 13:51:38 +0900

    Fix: 모달 수정

--- src/components/commons/basic/BasicModal.tsx
+++ src/components/commons/basic/BasicModal.tsx
@@ -42,7 +42,7 @@ export default function BasicModal({ children, onClose, className, width }: Basi
 			className="fixed inset-0 z-50 flex items-center justify-center bg-black/50 p-4"
 			onClick={closeModalOnBackgroundClick}>
 			<div
-				className={`relative min-w-[300px] rounded-lg bg-white shadow-xl`}
+				className={`relative rounded-lg bg-white p-[24px] shadow-xl`}
 				style={{ width: width }}
 				onClick={e => e.stopPropagation()}>
 				<button
