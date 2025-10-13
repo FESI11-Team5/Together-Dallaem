482ff3b0b9590275a3a3338aa5b8f571efd396f5
annann23 <annann23@naver.com>
2025-10-13 13:08:41 +0900

    Fix: Badge 컴포넌트 필요없는 Dom 삭제

--- src/components/commons/Badge.tsx
+++ src/components/commons/Badge.tsx
@@ -2,8 +2,9 @@ export default function Badge({ num }: { num: number }) {
 	const showText = num > 999 ? '999+' : num;
 
 	return (
-		<div className={`flex h-[17px] items-center rounded-[8.5px] bg-gray-900 px-[7px] py-0`}>
-			<span className={`text-xs leading-[16px] font-semibold text-white`}>{showText}</span>
+		<div
+			className={`inline-flex h-[17px] w-auto items-center justify-center rounded-[8.5px] bg-gray-900 px-[7px] py-0 text-xs leading-[16px] font-semibold text-white`}>
+			{showText}
 		</div>
 	);
 }
