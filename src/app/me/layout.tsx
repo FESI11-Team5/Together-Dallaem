8cb17e7393dc49a5d4a486c59e94191f98a9a106
y0unj1NoH <nohyoong711@gmail.com>
2025-10-14 10:33:24 +0900

    Chore: 페이지 가드 임시로 주석처리

--- src/app/me/layout.tsx
+++ src/app/me/layout.tsx
@@ -1,20 +1,20 @@
-'use client';
+// 'use client';
 
-import RequiredLoginPopup from '@/components/auth/Popup/RequiredLoginPopup';
-import { useModal } from '@/hooks/useModal';
-import { isAuthenticated } from '@/utils/token';
-import { usePathname } from 'next/navigation';
-import { useEffect } from 'react';
+// import RequiredLoginPopup from '@/components/auth/Popup/RequiredLoginPopup';
+// import { useModal } from '@/hooks/useModal';
+// import { isAuthenticated } from '@/utils/token';
+// import { usePathname } from 'next/navigation';
+// import { useEffect } from 'react';
 
-export default function MeLayout({ children }: { children: React.ReactNode }) {
-	const pathname = usePathname();
-	const { openModal } = useModal();
+// export default function MeLayout({ children }: { children: React.ReactNode }) {
+// 	const pathname = usePathname();
+// 	const { openModal } = useModal();
 
-	useEffect(() => {
-		if (!isAuthenticated()) {
-			openModal(<RequiredLoginPopup next={pathname} />);
-		}
-	}, [pathname]);
+// 	useEffect(() => {
+// 		if (!isAuthenticated()) {
+// 			openModal(<RequiredLoginPopup next={pathname} />);
+// 		}
+// 	}, [pathname]);
 
-	return <>{children}</>;
-}
+// 	return <>{children}</>;
+// }
