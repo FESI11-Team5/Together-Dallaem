926c4bf8beb31d4bb1b7d17ea986427c2e74bcb5
bbagbbagn2 <pyoungh999@naver.com>
2025-10-17 01:24:27 +0900

    Fix: reviews API withAuth 제거

--- src/apis/reviews/index.ts
+++ src/apis/reviews/index.ts
@@ -5,11 +5,16 @@ const REVIEWS_PATH = '/reviews';
 
 export const getReviews = async (): Promise<GetReviewsResponse> => {
 	return getRequest({
-		path: REVIEWS_PATH,
-		options: { withAuth: true }
+		path: REVIEWS_PATH
 	});
 };
 
+/**
+ * 팀 모임에 대한 리뷰를 추가합니다.
+ *
+ * @param reviewData - 리뷰 정보 (gatheringId, score, comment)
+ * @returns 작성된 리뷰 데이터
+ */
 export const postReviews = (reviewData: CreateReviewRequest) =>
 	postRequest<CreateReviewRequest>({
 		path: REVIEWS_PATH,
