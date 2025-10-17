1d510b71443ff2da5afaab39b55c29a1b0e85e3f
y0unj1NoH <nohyoong711@gmail.com>
2025-10-17 10:29:10 +0900

    Test: DropdownMenu 테스트 작성 (TDD)

--- src/components/commons/GNB/index.test.tsx
+++ src/components/commons/GNB/index.test.tsx
@@ -1,6 +1,6 @@
-import GNB from '.'
-import { render, screen, waitFor } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import userEvent, { UserEvent } from '@testing-library/user-event';
+import GNB from '.';
 
 describe('GNB 테스트', () => {
 	let user: UserEvent;
@@ -20,7 +20,7 @@ describe('GNB 테스트', () => {
 			expect(link).toHaveAttribute('href', '/');
 		});
 
-		test('"찜한 모임" 메뉴를 클릭하면 "/favorites"으로 이동한다', async() => {
+		test('"찜한 모임" 메뉴를 클릭하면 "/favorites"으로 이동한다', async () => {
 			// 1. 찜한 모임 메뉴 누르기
 			const link = screen.getByRole('link', { name: '찜한 모임' });
 			await user.click(link);
@@ -29,7 +29,7 @@ describe('GNB 테스트', () => {
 			expect(link).toHaveAttribute('href', '/favorites');
 		});
 
-		test('"모든 리뷰" 메뉴를 클릭하면 "/reviews"으로 이동한다',async() =>{
+		test('"모든 리뷰" 메뉴를 클릭하면 "/reviews"으로 이동한다', async () => {
 			// 1. 모든 리뷰 메뉴 누르기
 			const link = screen.getByRole('link', { name: '모든 리뷰' });
 			await user.click(link);
@@ -39,22 +39,26 @@ describe('GNB 테스트', () => {
 		});
 	});
 
-	describe('로그인 여부', () => {
+	describe('GNB 버튼 동작', () => {
 		describe('미로그인 상태', () => {
-            test('로그인 버튼이 보인다.', () => {
-            });
+			test('비로그인 사용자가 로그인 버튼 클릭 시 "/signin"으로 이동한다', async () => {
+				localStorage.getItem = jest.fn().mockReturnValue('');
 
-            test('로그인 버튼을 클릭하면 로그인 페이지로 이동한다.', () => {
-            });
-		});
+				// 1. 모임 찾기 메뉴 누르기
+				const link = screen.getByRole('link', { name: '로그인' });
+				await user.click(link);
 
-		describe('로그인 상태', () => {
-			test('로그인 시 드롭다운 버튼이 보인다', () => {});
+				// 2. 홈 페이지로 이동했는지 확인하기
+				expect(link).toHaveAttribute('href', '/signin');
+			});
 
-			test('드롭다운의 마이페이지 버튼을 클릭하면 "/me"로 이동한다', () => {});
+			test('로그인 사용자가 드롭다운(프로필 사진)의 마이페이지 버튼 클릭 시 "/me"로 이동한다', () => {
+				localStorage.getItem = jest.fn().mockReturnValue('token');
+			});
 
-			test('드롭다운의 로그아웃 버튼을 클릭하면 로그아웃된다', () => {});
+			test('로그인 사용자가 드롭다운(프로필 사진)의 로그아웃 버튼 클릭 시 로그아웃한다', () => {
+				localStorage.getItem = jest.fn().mockReturnValue('token');
+			});
 		});
 	});
-
 });
