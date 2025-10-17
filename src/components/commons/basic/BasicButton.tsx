bc99c060d509da19290c4453983bbc79f086a804
annann23 <annann23@naver.com>
2025-10-17 14:32:52 +0900

    Fix: BasicButton large 아닐 때 글자 크기 변경, font-weight 오타 수정

--- src/components/commons/basic/BasicButton.tsx
+++ src/components/commons/basic/BasicButton.tsx
@@ -89,9 +89,9 @@ export default function BasicButton({
 			{...rest}
 			//prettier-ignore
 			className={`
-				font-pretendard font-weight-semibold box-border
-				rounded-[12px] border-1 py-[10px] text-[16px] no-underline
-				${isLarge ? 'w-full' : 'w-[120px]'}
+				font-pretendard font-semibold box-border
+				rounded-[12px] border-1 py-[10px] no-underline
+				${isLarge ? 'w-full text-[16px]' : 'w-[120px] text-[14px]'}
 				${classByStatus}
 				${isActive ? 'cursor-pointer' : 'cursor-default'}
 				${className}
