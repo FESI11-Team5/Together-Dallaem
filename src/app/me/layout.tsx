24f7402b9025397c766e414d18061eea69c3d8b8
bbagbbagn2 <pyoungh999@naver.com>
2025-10-20 05:39:41 +0900

    Fix: useRef를 이용해서 모달 무한 루프 처리

--- src/app/me/layout.tsx
+++ src/app/me/layout.tsx
@@ -9,7 +9,6 @@ import RequiredLoginPopup from '@/components/auth/Popup/RequiredLoginPopup';
  *
  * 로그인된 사용자만 접근 가능한 페이지의 레이아웃을 제공합니다.
  * - 페이지 접근 시 사용자가 인증되어 있지 않으면 `RequiredLoginPopup` 모달을 표시합니다.
- * - 모달에는 현재 경로(`pathname`)를 전달하여 로그인 후 돌아올 수 있도록 지원합니다.
  *
  * @param {object} props - 컴포넌트 props
  * @param {React.ReactNode} props.children - 레이아웃 내부에 렌더링될 자식 컴포넌트
@@ -23,21 +22,22 @@ import RequiredLoginPopup from '@/components/auth/Popup/RequiredLoginPopup';
  * ```
  */
 export default function MeLayout({ children }: { children: React.ReactNode }) {
-	// const { isAuthenticated } = useAuth();
-	// const { openModal } = useModal();
+	const { isAuthenticated } = useAuth();
+	const { openModal } = useModal();
 
-	// const hasOpenedRef = useRef(false);
+	const hasOpenedRef = useRef(false);
 
-	// useEffect(() => {
-	// 	if (!isAuthenticated && !hasOpenedRef.current) {
-	// 		openModal(<RequiredLoginPopup next="/me" />);
-	// 		hasOpenedRef.current = true;
-	// 	}
-	// }, [isAuthenticated]);
+	useEffect(() => {
+		if (isAuthenticated === false && !hasOpenedRef.current) {
+			openModal(<RequiredLoginPopup next="/me" />);
+			hasOpenedRef.current = true;
+			console.log(isAuthenticated);
+		}
+	}, [isAuthenticated, openModal]);
 
-	// if (!isAuthenticated) {
-	// 	return <div className="box-border bg-gray-100"></div>;
-	// }
+	if (!isAuthenticated) {
+		return <div className="box-border bg-gray-100"></div>;
+	}
 
 	return children;
 }
