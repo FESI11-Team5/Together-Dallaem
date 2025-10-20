ada8614c604438515c918590b507198eee791c80
y0unj1NoH <nohyoong711@gmail.com>
2025-10-20 10:51:49 +0900

    Feat: GatheringFilterBar에 SortButton 추가

--- src/app/(home)/GatheringFilterBar/index.tsx
+++ src/app/(home)/GatheringFilterBar/index.tsx
@@ -1,11 +1,15 @@
 'use client';
 
-import FilterSelectGroup from '@/app/(home)/FilterSelectGroup';
 import GatheringTabs from '@/app/(home)/GatheringTabs';
+import SearchCalendar from '@/app/(home)/SearchCalendar';
 import BasicButton from '@/components/commons/basic/BasicButton';
+import SelectBox from '@/components/commons/SelectBox';
+import SortButton from '@/components/commons/SortButton';
 import GatheringModal from '@/components/gatherings/GatheringModal';
+import { LOCATION_OPTIONS, SORT_OPTIONS } from '@/constants/options';
 import { useModal } from '@/hooks/useModal';
 import { Dispatch, SetStateAction, useEffect, useState } from 'react';
+import { Controller, FormProvider, useForm, type UseFormRegisterReturn } from 'react-hook-form';
 
 interface GatheringFilterBarProps {
 	setFilterCriteria: Dispatch<
@@ -13,8 +17,13 @@ interface GatheringFilterBarProps {
 			type: string;
 			location: string | number;
 			date?: Date;
+			sort: string;
 		}>
 	>;
+}
+
+interface SortFormValues {
+	sort: string;
 }
 
 export default function GatheringFilterBar({ setFilterCriteria }: GatheringFilterBarProps) {
@@ -22,28 +31,62 @@ export default function GatheringFilterBar({ setFilterCriteria }: GatheringFilte
 	const [selectedType, setSelectedType] = useState<string>('');
 	const [selectedLocation, setSelectedLocation] = useState<string | number>('');
 	const [selectedDate, setSelectedDate] = useState<Date>();
-	// const [selectedSort, setSelectedSort] = useState<string>();
+
+	const methods = useForm<SortFormValues>({
+		defaultValues: { sort: 'deadlineLate' }
+	});
+	const { control, watch } = methods;
+	const selectedSort = watch('sort');
 
 	useEffect(() => {
 		setFilterCriteria({
 			type: selectedType,
 			location: selectedLocation,
-			date: selectedDate
+			date: selectedDate,
+			sort: selectedSort
 		});
-	}, [selectedType, selectedLocation, selectedDate, setFilterCriteria]);
+	}, [selectedType, selectedLocation, selectedDate, selectedSort, setFilterCriteria]);
 
 	return (
-		<div className="flex w-full flex-col gap-4">
-			<GatheringTabs
-				setSelectedType={setSelectedType}
-				button={<BasicButton onClick={() => openModal(<GatheringModal />)}>모임 만들기</BasicButton>}
-			/>
-			<hr />
-			<FilterSelectGroup
+		<FormProvider {...methods}>
+			<div className="flex w-full flex-col gap-4">
+				<GatheringTabs
+					setSelectedType={setSelectedType}
+					button={<BasicButton onClick={() => openModal(<GatheringModal />)}>모임 만들기</BasicButton>}
+				/>
+				<hr />
+
+				<div className="flex w-full justify-between">
+					<div className="flex gap-2">
+						{/* // TODO: 반응형 부분은 동적으로 불러오는 거 말고 브레이크 포인트로 가능한지 논의 */}
+						<SelectBox options={LOCATION_OPTIONS} placeholder="지역 전체" size="small" onChange={setSelectedLocation} />
+						<SearchCalendar date={selectedDate} setDate={setSelectedDate} />
+					</div>
+					<Controller
+						name="sort"
+						control={control}
+						render={({ field }) => (
+							<SortButton
+								options={SORT_OPTIONS}
+								defaultValue={field.value}
+								register={
+									{
+										name: field.name,
+										onChange: field.onChange,
+										onBlur: field.onBlur,
+										ref: field.ref
+									} as unknown as UseFormRegisterReturn
+								}
+							/>
+						)}
+					/>
+				</div>
+				{/* <FilterSelectGroup
 				setSelectedLocation={setSelectedLocation}
 				selectedDate={selectedDate}
 				setSelectedDate={setSelectedDate}
-			/>
-		</div>
+			/> */}
+			</div>
+		</FormProvider>
 	);
 }
