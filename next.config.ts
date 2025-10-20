2e90b650198138d6de2d3faf6dd0da4abdc331ae
bbagbbagn2 <pyoungh999@naver.com>
2025-10-20 17:15:31 +0900

    Chore: 충돌 병합

--- next.config.ts
+++ next.config.ts
@@ -3,17 +3,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
 	/* config options here */
 	images: {
-<<<<<<< HEAD
-		remotePatterns: [
-			{
-				protocol: 'https',
-				hostname: 'sprint-fe-project.s3.ap-northeast-2.amazonaws.com',
-				pathname: '/**' // 모든 경로 허용
-			}
-		]
-=======
 		remotePatterns: [new URL('https://sprint-fe-project.s3.ap-northeast-2.amazonaws.com/**')]
->>>>>>> origin/TD-3
 	}
 };
 
