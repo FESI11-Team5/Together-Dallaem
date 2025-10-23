7218b5f7798cea401f9517d7ae9559174026065e
y0unj1NoH <138647668+y0unj1NoH@users.noreply.github.com>
2025-10-23 16:52:30 +0900

    TD-186 마이페이지 페이지 가드 버그 수정 (#31)
    * Feat: 하이드레이션 확인 로직 추가
    
    * TD-188 Fix: 마이페이지 페이지 가드 버그 수정

--- src/app/me/layout.tsx
+++ src/app/me/layout.tsx
@@ -1,9 +1,9 @@
 'use client';
 
-import { useEffect, useRef } from 'react';
+import RequiredLoginPopup from '@/components/auth/Popup/RequiredLoginPopup';
 import { useAuth } from '@/hooks/useAuth';
 import { useModal } from '@/hooks/useModal';
-import RequiredLoginPopup from '@/components/auth/Popup/RequiredLoginPopup';
+import { useEffect, useRef } from 'react';
 /**
  * `MeLayout` 컴포넌트
  *
@@ -13,30 +13,29 @@ import RequiredLoginPopup from '@/components/auth/Popup/RequiredLoginPopup';
  * @param {object} props - 컴포넌트 props
  * @param {React.ReactNode} props.children - 레이아웃 내부에 렌더링될 자식 컴포넌트
  * @returns {JSX.Element} 자식 컴포넌트를 포함한 레이아웃
- *
- * @example
- * ```tsx
- * <MeLayout>
- *   <UserProfile />
- * </MeLayout>
- * ```
  */
+// TODO: 페이지 가드에서 미들웨어로 바꾸기
 export default function MeLayout({ children }: { children: React.ReactNode }) {
 	const { isAuthenticated } = useAuth();
 	const { openModal } = useModal();
-
 	const hasOpenedRef = useRef(false);
 
 	useEffect(() => {
-		if (isAuthenticated === false && !hasOpenedRef.current) {
+		if (isAuthenticated === null) return;
+		if (!isAuthenticated && !hasOpenedRef.current) {
 			openModal(<RequiredLoginPopup next="/me" />);
 			hasOpenedRef.current = true;
-			console.log(isAuthenticated);
 		}
-	}, [isAuthenticated, openModal]);
+	}, [isAuthenticated]);
 
+	// TODO: div에 스켈레톤이 들어가면 좋을 것 같아요 (아니면 로딩 느낌으로?)
+	// 지금은 혹시 몰라서 각자 경우를 나눴는데 지훈님이 원하시는 대로 하시면 될 것 같아요
+	// 하이드레이션 중 (인증 확인 중)
+	if (isAuthenticated === null) return <div>인증 확인중...</div>;
+
+	// 하이드레이션 후 로그인이 안된 경우
 	if (!isAuthenticated) {
-		return <div className="box-border bg-gray-100"></div>;
+		return <div className="box-border bg-gray-100">인증 실패...</div>;
 	}
 
 	return children;
--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,5 +1,6 @@
 import { useUserStore } from '@/stores/user';
 import { isTokenExpired } from '@/utils/token';
+import { useEffect, useState } from 'react';
 
 /**
  * useAuth
@@ -7,8 +8,17 @@ import { isTokenExpired } from '@/utils/token';
  * - Zustand의 userStore를 기반으로 인증 여부 및 토큰을 반환함
  */
 export function useAuth() {
+	const hasHydrated = useUserStore(state => state.hasHydrated);
 	const token = useUserStore(state => state.user?.token);
-	const isAuthenticated = !!token && isTokenExpired(token) !== 'EXPIRED';
+	const signoutUser = useUserStore(state => state.signoutUser);
+	const [isAuthenticated, setIsAuthenticated] = useState<boolean | null>(null);
+
+	useEffect(() => {
+		if (hasHydrated === null) return;
+		const check = !!token && isTokenExpired(token) !== 'EXPIRED';
+		setIsAuthenticated(check);
+		if (!check) signoutUser();
+	}, [hasHydrated, token]);
 
 	return {
 		token,
