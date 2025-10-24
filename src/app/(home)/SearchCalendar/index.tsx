55499a784c015e071fdd9eada1ec6fe5edcf925d
y0unj1NoH <nohyoong711@gmail.com>
2025-10-24 13:34:42 +0900

    Fix: 캘린더 버튼 SearchCalendar에 합체

--- src/app/(home)/SearchCalendar/index.tsx
+++ src/app/(home)/SearchCalendar/index.tsx
@@ -1,11 +1,11 @@
 'use client';
 
+import BasicButton from '@/components/commons/basic/BasicButton';
 import BasicSelectButton from '@/components/commons/basic/BasicSelectButton';
 import { Calendar } from '@/components/ui/calendar';
 import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
 import { format } from 'date-fns';
 import { Dispatch, SetStateAction, useMemo, useState } from 'react';
-import SearchCalendarButton from './SearchCalendarButton';
 
 interface SearchCalendarProps {
 	/** 선택된 날짜 (없을 수 있음) */
@@ -15,7 +15,7 @@ interface SearchCalendarProps {
 }
 
 // TODO: UI 디자인 시안대로 수정
-// TODO: 적용누르면 그때 onChange 실행되는 걸로 수정
+// TODO: 대신 날짜 선택하면 바로 되고, 적용은 닫기로 바꾸는 거 논의
 /**
  * 날짜를 선택할 수 있는 캘린더 선택 컴포넌트
  * 버튼 클릭 시 달력이 팝오버 형태로 열리며, 날짜 선택 시 상위 컴포넌트의 상태가 업데이트됩니다.
@@ -32,6 +32,12 @@ export default function SearchCalendar({ date, setDate }: SearchCalendarProps) {
 
 	const handleDateSelect = (selectedDate?: Date) => {
 		setDate(selectedDate);
+	};
+
+	const handleApply = () => {
+		if (!date) return;
+		setDate(date);
+		setIsOpen(false);
 	};
 
 	return (
@@ -60,7 +66,19 @@ export default function SearchCalendar({ date, setDate }: SearchCalendarProps) {
 							weekday: 'font-bold text-black flex-1'
 						}}
 					/>
-					<SearchCalendarButton date={date} setDate={setDate} setIsOpen={setIsOpen} />
+					<div className="mt-2 flex w-full gap-3">
+						<BasicButton
+							outlined
+							onClick={() => {
+								setDate(undefined);
+							}}
+							disabled={date === undefined}>
+							초기화
+						</BasicButton>
+						<BasicButton onClick={handleApply} disabled={date === undefined}>
+							적용
+						</BasicButton>
+					</div>
 				</div>
 			</PopoverContent>
 		</Popover>
