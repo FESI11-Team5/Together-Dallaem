1bbb2b31edd050971bf4950ce2e07bf45f2f2cd8
y0unj1NoH <nohyoong711@gmail.com>
2025-10-28 17:08:27 +0900

    TD-235 Fix: GNB 컬러 변경

--- src/components/commons/GNB/index.tsx
+++ src/components/commons/GNB/index.tsx
@@ -67,56 +67,60 @@ export default function GNB() {
 		router.push(path);
 	};
 
+	// TODO: 구조 정리하기
 	return (
-		<header className="mb:h-15 mb:px-6 z-layout sticky top-0 flex h-14 w-full items-center justify-center border-b-2 border-gray-900 bg-primary-600 px-4">
-			<div className="tb:max-w-300 flex w-full justify-between">
-				<div className="mb:gap-6 flex items-center gap-5">
-					<h1 className="flex items-center text-lg leading-none font-extrabold text-white">
-						<Link href="/" className="align-middle">
-							같이 달램
-						</Link>
-					</h1>
-					<nav className="mb:text-base mb:gap-6 flex items-center gap-3 text-sm leading-none font-semibold">
-						{NAVBAR_MENU_LINKS.map(({ href, label }) => (
-							<Link
-								key={href}
-								href={href}
-								className={cn(
-									'align-middle transition-colors hover:text-gray-800',
-									pathname === href ? 'text-gray-900' : 'text-primary-50'
-								)}>
-								{label}
+		<>
+			<header className="mb:h-15 mb:px-6 z-layout bg-bg sticky top-0 flex h-14 w-full items-center justify-center px-4">
+				<div className="tb:max-w-300 flex w-full justify-between">
+					<div className="mb:gap-6 flex items-center gap-5">
+						<h1 className="flex items-center text-lg leading-none font-extrabold text-white">
+							<Link href="/" className="align-middle">
+								같이 달램
 							</Link>
-						))}
-					</nav>
-				</div>
-
-				{isAuthenticated ? (
-					<DropdownMenu>
-						<DropdownMenu.Trigger>
-							<div className="relative size-[40px] overflow-hidden rounded-full">
-								<Image
-									priority
-									src={user?.image || '/images/profile.svg'}
-									alt="프로필 사진"
-									fill
-									className="object-cover"
-								/>
-							</div>
-						</DropdownMenu.Trigger>
-						<DropdownMenu.Items options={DROPDOWN_MENU_OPTIONS} onClick={handleDropdownMenuClick} />
-					</DropdownMenu>
-				) : (
-					// TODO: 너무 마음에 안듭니다... 나중에 수정할게요...
-					<div
-						role="button"
-						tabIndex={0}
-						onClick={handleSigninClick}
-						className="leading-sm mb:leading-base mb:text-base cursor-pointer text-sm font-semibold text-white">
-						로그인
+						</h1>
+						<nav className="mb:text-base mb:gap-6 flex items-center gap-3 text-sm leading-none font-semibold">
+							{NAVBAR_MENU_LINKS.map(({ href, label }) => (
+								<Link
+									key={href}
+									href={href}
+									className={cn(
+										'align-middle transition-colors hover:text-gray-800',
+										pathname === href ? 'text-gray-900' : 'text-primary-50'
+									)}>
+									{label}
+								</Link>
+							))}
+						</nav>
 					</div>
-				)}
-			</div>
-		</header>
+
+					{isAuthenticated ? (
+						<DropdownMenu>
+							<DropdownMenu.Trigger>
+								<div className="relative size-[40px] overflow-hidden rounded-full">
+									<Image
+										priority
+										src={user?.image || '/images/profile.svg'}
+										alt="프로필 사진"
+										fill
+										className="object-cover"
+									/>
+								</div>
+							</DropdownMenu.Trigger>
+							<DropdownMenu.Items options={DROPDOWN_MENU_OPTIONS} onClick={handleDropdownMenuClick} />
+						</DropdownMenu>
+					) : (
+						// TODO: 너무 마음에 안듭니다... 나중에 수정할게요...
+						<div
+							role="button"
+							tabIndex={0}
+							onClick={handleSigninClick}
+							className="leading-sm mb:leading-base mb:text-base cursor-pointer text-sm font-semibold text-white">
+							로그인
+						</div>
+					)}
+				</div>
+			</header>
+			<div aria-hidden className="from-primary-500 to-highlight right-0 bottom-0 left-0 h-1 bg-gradient-to-r"></div>
+		</>
 	);
 }
