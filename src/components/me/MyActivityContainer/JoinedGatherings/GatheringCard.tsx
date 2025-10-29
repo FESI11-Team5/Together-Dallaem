40844bd183dbdf8b569a5e8bf7d47108b73b806d
bbagbbagn2 <pyoungh999@naver.com>
2025-10-29 09:53:31 +0900

    Chore: 버튼 임의 hover 삭제

--- src/components/me/MyActivityContainer/JoinedGatherings/GatheringCard.tsx
+++ src/components/me/MyActivityContainer/JoinedGatherings/GatheringCard.tsx
@@ -87,9 +87,7 @@ export default function GatheringCard({ gathering, onReviewSuccess, onCancelSucc
 					) : (
 						<BasicButton
 							className={`w-auto min-w-30 px-[22px] transition-colors ${
-								gathering.isReviewed
-									? 'cursor-not-allowed !bg-gray-200 !text-gray-500'
-									: 'hover:bg-orange-700 active:bg-orange-800'
+								gathering.isReviewed && 'cursor-not-allowed !bg-gray-200 !text-gray-500'
 							}`}
 							onClick={!gathering.isReviewed ? handleAddReviewClick : undefined}
 							isActive={!gathering.isReviewed}>
