d390f956b1b093ab5f5cf325b840f4a44e0148db
bbagbbagn2 <pyoungh999@naver.com>
2025-10-29 16:36:11 +0900

    Chore: div 태그 대신 main, h1 태그로 변경

--- src/app/me/page.tsx
+++ src/app/me/page.tsx
@@ -1,14 +1,14 @@
-import ProfileEditCard from '@/components/me/ProfileEditCard';
+import ProfileEditCard from '@/components/me/ProfileEditContainer/ProfileEditCard';
 import MyActivityContainer from '@/components/me/MyActivityContainer';
 
 export default function Me() {
 	return (
-		<div className="box-border bg-gray-100" style={{ fontFamily: 'var(--font-pretendard)' }}>
+		<main className="box-border bg-white/15" style={{ fontFamily: 'var(--font-pretendard)' }}>
 			<div className="tb:px-6 tb:pt-8 pc:max-w-300 pc:px-25 bg-base m-auto flex min-h-[calc(100vh-60px)] flex-col px-4 pt-6">
-				<div className="mb-4 text-lg font-semibold text-gray-900">마이페이지</div>
+				<h1 className="text-primary-500 mb-4 text-lg font-semibold">마이페이지</h1>
 				<ProfileEditCard />
 				<MyActivityContainer />
 			</div>
-		</div>
+		</main>
 	);
 }
