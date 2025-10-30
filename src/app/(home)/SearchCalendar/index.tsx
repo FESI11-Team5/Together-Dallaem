d7a20f0c4cb6b0be42b86215cd6fdb5fa7618475
y0unj1NoH <nohyoong711@gmail.com>
2025-10-30 09:48:24 +0900

    TD-200 Fix: 캘린더 적용을 눌렀을 때만 날짜가 바뀌도록 수정

--- src/app/(home)/SearchCalendar/index.tsx
+++ src/app/(home)/SearchCalendar/index.tsx
@@ -5,7 +5,7 @@ import BasicSelectButton from '@/components/commons/basic/BasicSelectButton';
 import { Calendar } from '@/components/ui/calendar';
 import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
 import { format } from 'date-fns';
-import { Dispatch, SetStateAction, useMemo, useState } from 'react';
+import { Dispatch, SetStateAction, useEffect, useMemo, useState } from 'react';
 
 interface SearchCalendarProps {
 	/** 선택된 날짜 (없을 수 있음) */
@@ -24,21 +24,32 @@ interface SearchCalendarProps {
  */
 export default function SearchCalendar({ date, setDate }: SearchCalendarProps) {
 	const [isOpen, setIsOpen] = useState(false);
+	const [tempDate, setTempDate] = useState<Date | undefined>(date);
 
 	const formattedDate = useMemo(() => {
 		if (!date) return undefined;
 		return format(date, 'yy/MM/dd');
 	}, [date]);
 
-	const handleDateSelect = (selectedDate?: Date) => {
-		setDate(selectedDate);
+	const handleDateSelect = (date?: Date) => {
+		setTempDate(date);
 	};
 
 	const handleApply = () => {
-		if (!date) return;
-		setDate(date);
+		if (!tempDate) return;
+		setDate(tempDate);
 		setIsOpen(false);
 	};
+
+	const handleReset = () => {
+		setTempDate(undefined);
+		setDate(undefined);
+	};
+
+	useEffect(() => {
+		if (isOpen) return;
+		setTempDate(undefined);
+	}, [isOpen]);
 
 	return (
 		<Popover open={isOpen} onOpenChange={setIsOpen}>
@@ -55,7 +66,7 @@ export default function SearchCalendar({ date, setDate }: SearchCalendarProps) {
 				<div className="flex w-[250px] flex-col">
 					<Calendar
 						mode="single"
-						selected={date}
+						selected={tempDate}
 						onSelect={handleDateSelect}
 						formatters={{
 							formatWeekdayName: (date, options) => format(date, 'EEE', { locale: options?.locale })
@@ -68,15 +79,10 @@ export default function SearchCalendar({ date, setDate }: SearchCalendarProps) {
 						}}
 					/>
 					<div className="mt-2 flex w-full gap-3">
-						<BasicButton
-							outlined
-							onClick={() => {
-								setDate(undefined);
-							}}
-							disabled={date === undefined}>
+						<BasicButton outlined onClick={handleReset} disabled={date === undefined}>
 							초기화
 						</BasicButton>
-						<BasicButton onClick={handleApply} disabled={date === undefined}>
+						<BasicButton onClick={handleApply} disabled={tempDate === undefined}>
 							적용
 						</BasicButton>
 					</div>
