03482de737bef828920fcb991d27e5cd97c3752f
y0unj1NoH <nohyoong711@gmail.com>
2025-10-31 17:10:06 +0900

    Chore: TODO 제거

--- src/app/(home)/SearchCalendar/index.tsx
+++ src/app/(home)/SearchCalendar/index.tsx
@@ -14,8 +14,6 @@ interface SearchCalendarProps {
 	setDate: Dispatch<SetStateAction<Date | undefined>>;
 }
 
-// TODO: UI 디자인 시안대로 수정
-// TODO: 대신 날짜 선택하면 바로 되고, 적용은 닫기로 바꾸는 거 논의
 /**
  * 날짜를 선택할 수 있는 캘린더 선택 컴포넌트
  * 버튼 클릭 시 달력이 팝오버 형태로 열리며, 날짜 선택 시 상위 컴포넌트의 상태가 업데이트됩니다.
