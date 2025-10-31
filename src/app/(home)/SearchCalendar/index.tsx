b51da1827eb2ad613a982b1d6cae66bda3b202ae
y0unj1NoH <nohyoong711@gmail.com>
2025-10-31 15:41:11 +0900

    TD-199 Fix: 캘린더 UI 수정

--- src/app/(home)/SearchCalendar/index.tsx
+++ src/app/(home)/SearchCalendar/index.tsx
@@ -62,7 +62,10 @@ export default function SearchCalendar({ date, setDate }: SearchCalendarProps) {
 					onClick={() => setIsOpen(prev => !prev)}
 				/>
 			</PopoverTrigger>
-			<PopoverContent className="flex w-[326px] justify-center" align="start" isModal={false}>
+			<PopoverContent
+				className="bg-root mr-4 flex min-w-[300px] flex-col items-center justify-center rounded-[12px] border-2 border-gray-200"
+				align="start"
+				isModal={false}>
 				<div className="flex w-[250px] flex-col">
 					<Calendar
 						mode="single"
@@ -71,12 +74,20 @@ export default function SearchCalendar({ date, setDate }: SearchCalendarProps) {
 						formatters={{
 							formatWeekdayName: (date, options) => format(date, 'EEE', { locale: options?.locale })
 						}}
-						// TODO: 다른 달력들과 색상 통일하기
+						// TODO: 달력 다시 학습하기
 						classNames={{
-							day: 'text-sm hover:bg-gray-100',
-							today: 'text-primary-500 rounded-md',
-							weekday: 'font-bold text-black flex-1'
+							day: 'text-primary-50 hover:bg-primary-500/10 transition-colors',
+							today: 'font-bold text-primary-500',
+							selected: 'bg-primary-500 text-primary-50 rounded-md',
+							outside: 'text-primary-700 opacity-80',
+							weekday: 'font-bold text-sm text-primary-50 flex-1 justify-between',
+							month_caption: 'relative flex items-center justify-center mb-2 pointer-events-none',
+							caption_label: 'text-sm font-bold text-primary-500 pointer-events-auto',
+							nav: 'absolute left-0 top-0 w-full flex justify-between items-center gap-1 z-10 pointer-events-auto',
+							button_next: 'text-primary-500 hover:text-primary-400 transition-colors rounded-full p-1',
+							button_previous: 'text-primary-500 hover:text-primary-400 transition-colors rounded-full p-1'
 						}}
+						fixedWeeks
 					/>
 					<div className="mt-2 flex w-full gap-3">
 						<BasicButton outlined onClick={handleReset} disabled={date === undefined}>
