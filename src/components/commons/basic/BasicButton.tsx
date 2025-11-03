8c5f0516cf6b277d462f0e4af5be4b713c25286c
annann23 <annann23@naver.com>
2025-11-03 16:48:54 +0900

    Fix: BasicButton relative 스타일 삭제

--- src/components/commons/basic/BasicButton.tsx
+++ src/components/commons/basic/BasicButton.tsx
@@ -122,7 +122,7 @@ export default function BasicButton({
 	const variant = outlined ? 'outlined' : 'solid';
 
 	return (
-		<div className={`group ${className} relative ${isLarge ? 'w-full' : 'w-[120px]'}`}>
+		<div className={`group ${className} ${isLarge ? 'w-full' : 'w-[120px]'}`}>
 			<div className={buttonShadowVariants({ variant: variant as 'solid' | 'outlined' })}></div>
 			<button
 				onClick={onClick}
